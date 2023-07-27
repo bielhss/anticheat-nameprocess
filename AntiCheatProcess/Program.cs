919597067218985ba4a33cf1efbff10663e68518
Gabriel Henrique <bielhsss1@gmail.com>
2023-07-27 11:03:39 -0300

    first commit

---
+++ AntiCheatProcess/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Threading;
+
+class AntiCheat
+{
+    private static readonly List<string> ForbiddenProcesses = new List<string>
+    {
+        "cheatengine",
+        "gamehackingtool",
+        "memoryhackingtool"
+    };
+
+    public static bool IsCheatProcessRunning()
+    {
+
+
+        Process[] processes = Process.GetProcesses();
+
+       
+        foreach (Process process in processes)
+        {
+            foreach (string forbiddenProcess in ForbiddenProcesses)
+            {
+                if (process.ProcessName.ToLower().Contains(forbiddenProcess))
+                {
+                    return true; // Um processo de trapaça foi encontrado
+                }
+            }
+        }
+
+        return false; // Nenhum processo de trapaça foi encontrado
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Process [] assaultCube = Process.GetProcessesByName("ac_client");
+        //Process assaultCube = Process.Start("ac_client");
+        //Console.WriteLine("Iniciando Processo assault cube Id = " + assaultCube.Id);
+
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AssaultCube\AssaultCube.lnk",
+            UseShellExecute = true
+        };
+       
+        
+        Process assaultCube = Process.Start(processStartInfo);
+
+        int PID = assaultCube.Id;
+
+       
+
+
+        Process x = Process.GetProcessById(PID);
+        Console.WriteLine("O PID é = " + PID);
+        Console.WriteLine("Iniciando o jogo...");
+
+        while (true)
+        {
+
+                if (AntiCheat.IsCheatProcessRunning())
+                {
+                    Console.WriteLine("Processo de trapaça encontrado. Encerrando o jogo...");
+                    foreach (var p in Process.GetProcessesByName("ac_client"))
+                    {
+                        p.Kill();
+                        p.WaitForExit();
+                    }
+                }
+
+                // Lógica do jogo...
+
+                // Aguarda um curto período de tempo antes de verificar novamente
+                Thread.Sleep(1000); // 1 segundo
+            }
+    }
+}
